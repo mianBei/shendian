24a328a48d29d1a37011082aa4298dbe165c3781
syw06 <809027571@qq.com>
2020-07-04 14:14:08 +0800

    初始代码

--- manager/manager-api/src/main/java/com/example/managerapi/controller/testController.java
+++ manager/manager-api/src/main/java/com/example/managerapi/controller/testController.java
@@ -4,7 +4,15 @@ import com.example.managerDao.user.entity.AccountInfo;
 import com.example.managerService.user.IAccountInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +22,32 @@ import java.util.List;
 @RestController
 @RequestMapping("/test")
 public class testController {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Value("${foo}")
+    private String text;
+    @GetMapping("/say")
+    public String sayHello(){
+        return text;
+    }
     @RequestMapping(value = "/test.htm",method = RequestMethod.GET)
     public String test(){
 
         return "test";
     }
+    @RequestMapping(value = "/test1.htm",method = RequestMethod.GET)
+    public void test1() throws Exception {
+        stringRedisTemplate.opsForValue().set("aaa", "111");
+        Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+
+    }
+/*    @RequestMapping("/getUser")
+    @Cacheable(value="user-key")
+    public User getUser() {
+        User user=new User("aa@126.com", "aa", "aa123456", "aa","123");
+        System.out.println("若下面没出现“无缓存的时候调用”字样且能打印出数据表示测试成功");
+        return user;
+    }*/
 }
